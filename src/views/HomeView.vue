c0fc0cc40dd460ca1fc639d5566c0cd52f1c6bb9
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-09 11:39:35 +0100

    REFACTOR: cleans up styles, removes starter components, minor tweaks to styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl font-bold underline"> Hello world! </h1>
+    <i class="fas fa-thumbs-up fa-5x"></i>
   </main>
 </template>
