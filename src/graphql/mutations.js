39c9126b7a69c687775520cb4cfe273277cc09ee
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-12 14:10:28 +0100

    FEAT (amplify): adds GraphQL AppSync methods using codegen from Amplify

---
+++ src/graphql/mutations.js
@@ -0,0 +1,218 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const editMyProfile = /* GraphQL */ `
+  mutation EditMyProfile($newProfile: ProfileInput!) {
+    editMyProfile(newProfile: $newProfile) {
+      id
+      name
+      screenName
+      imgUrl
+      bgImgUrl
+      bio
+      location
+      website
+      birthdate
+      createdAt
+      tweets {
+        nextToken
+        __typename
+      }
+      followersCount
+      followingCount
+      tweetsCount
+      likesCount
+      __typename
+    }
+  }
+`;
+export const tweet = /* GraphQL */ `
+  mutation Tweet($text: String!) {
+    tweet(text: $text) {
+      id
+      profile {
+        id
+        name
+        screenName
+        imgUrl
+        bgImgUrl
+        bio
+        location
+        website
+        birthdate
+        createdAt
+        followersCount
+        followingCount
+        tweetsCount
+        likesCount
+
+        ... on OtherProfile {
+          following
+          followedBy
+        }
+      }
+      createdAt
+      text
+      replies
+      likes
+      retweets
+      liked
+      retweeted
+      __typename
+    }
+  }
+`;
+export const like = /* GraphQL */ `
+  mutation Like($tweetId: ID!) {
+    like(tweetId: $tweetId)
+  }
+`;
+export const unlike = /* GraphQL */ `
+  mutation Unlike($tweetId: ID!) {
+    unlike(tweetId: $tweetId)
+  }
+`;
+export const retweet = /* GraphQL */ `
+  mutation Retweet($tweetId: ID!) {
+    retweet(tweetId: $tweetId) {
+      id
+      profile {
+        id
+        name
+        screenName
+        imgUrl
+        bgImgUrl
+        bio
+        location
+        website
+        birthdate
+        createdAt
+        followersCount
+        followingCount
+        tweetsCount
+        likesCount
+
+        ... on OtherProfile {
+          following
+          followedBy
+        }
+      }
+      createdAt
+      retweetOf {
+        id
+        createdAt
+
+        ... on Tweet {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+        ... on Reply {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+      }
+      __typename
+    }
+  }
+`;
+export const unretweet = /* GraphQL */ `
+  mutation Unretweet($tweetId: ID!) {
+    unretweet(tweetId: $tweetId)
+  }
+`;
+export const reply = /* GraphQL */ `
+  mutation Reply($tweetId: ID!, $text: String!) {
+    reply(tweetId: $tweetId, text: $text) {
+      id
+      profile {
+        id
+        name
+        screenName
+        imgUrl
+        bgImgUrl
+        bio
+        location
+        website
+        birthdate
+        createdAt
+        followersCount
+        followingCount
+        tweetsCount
+        likesCount
+
+        ... on OtherProfile {
+          following
+          followedBy
+        }
+      }
+      createdAt
+      text
+      replies
+      likes
+      retweets
+      liked
+      retweeted
+      inReplyToTweet {
+        id
+        createdAt
+
+        ... on Tweet {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+        ... on Reply {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+      }
+      inReplyToUsers {
+        id
+        name
+        screenName
+        imgUrl
+        bgImgUrl
+        bio
+        location
+        website
+        birthdate
+        createdAt
+        followersCount
+        followingCount
+        tweetsCount
+        likesCount
+
+        ... on OtherProfile {
+          following
+          followedBy
+        }
+      }
+      __typename
+    }
+  }
+`;
+export const follow = /* GraphQL */ `
+  mutation Follow($userId: ID!) {
+    follow(userId: $userId)
+  }
+`;
+export const unfollow = /* GraphQL */ `
+  mutation Unfollow($userId: ID!) {
+    unfollow(userId: $userId)
+  }
+`;
---
+++ src/graphql/queries.js
@@ -0,0 +1,203 @@
+/* eslint-disable */
+export const getImageUploadUrl = /* GraphQL */ `
+  query GetImageUploadUrl($extension: String, $contentType: String) {
+    getImageUploadUrl(extension: $extension, contentType: $contentType)
+  }
+`;
+export const getMyProfile = /* GraphQL */ `
+  query GetMyProfile {
+    getMyProfile {
+      id
+      name
+      screenName
+      imgUrl
+      bgImgUrl
+      bio
+      location
+      website
+      birthdate
+      createdAt
+      tweets {
+        nextToken
+        __typename
+      }
+      followersCount
+      followingCount
+      tweetsCount
+      likesCount
+      __typename
+    }
+  }
+`;
+export const getMyTimeline = /* GraphQL */ `
+  query GetMyTimeline($limit: Int!, $nextToken: String) {
+    getMyTimeline(limit: $limit, nextToken: $nextToken) {
+      tweets {
+        id
+        createdAt
+
+        ... on Tweet {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+        ... on Reply {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getProfile = /* GraphQL */ `
+  query GetProfile($screenName: String!) {
+    getProfile(screenName: $screenName) {
+      id
+      name
+      screenName
+      imgUrl
+      bgImgUrl
+      bio
+      location
+      website
+      birthdate
+      createdAt
+      tweets {
+        nextToken
+        __typename
+      }
+      followersCount
+      followingCount
+      tweetsCount
+      likesCount
+      following
+      followedBy
+      __typename
+    }
+  }
+`;
+export const getTweets = /* GraphQL */ `
+  query GetTweets($userId: ID!, $limit: Int!, $nextToken: String) {
+    getTweets(userId: $userId, limit: $limit, nextToken: $nextToken) {
+      tweets {
+        id
+        createdAt
+
+        ... on Tweet {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+        ... on Reply {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getLikes = /* GraphQL */ `
+  query GetLikes($userId: ID!, $limit: Int!, $nextToken: String) {
+    getLikes(userId: $userId, limit: $limit, nextToken: $nextToken) {
+      tweets {
+        id
+        createdAt
+
+        ... on Tweet {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+        ... on Reply {
+          text
+          replies
+          likes
+          retweets
+          liked
+          retweeted
+        }
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getFollowers = /* GraphQL */ `
+  query GetFollowers($userId: ID!, $limit: Int!, $nextToken: String) {
+    getFollowers(userId: $userId, limit: $limit, nextToken: $nextToken) {
+      profiles {
+        id
+        name
+        screenName
+        imgUrl
+        bgImgUrl
+        bio
+        location
+        website
+        birthdate
+        createdAt
+        followersCount
+        followingCount
+        tweetsCount
+        likesCount
+
+        ... on OtherProfile {
+          following
+          followedBy
+        }
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const getFollowing = /* GraphQL */ `
+  query GetFollowing($userId: ID!, $limit: Int!, $nextToken: String) {
+    getFollowing(userId: $userId, limit: $limit, nextToken: $nextToken) {
+      profiles {
+        id
+        name
+        screenName
+        imgUrl
+        bgImgUrl
+        bio
+        location
+        website
+        birthdate
+        createdAt
+        followersCount
+        followingCount
+        tweetsCount
+        likesCount
+
+        ... on OtherProfile {
+          following
+          followedBy
+        }
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
