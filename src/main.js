706094c23c55458974444cff0f86e3c96ecafc17
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-12 16:23:35 +0100

    FEAT (amplify): adds Amplify configuration to main.js and registers all the amplify-ui-vue within the app

--- src/main.js
+++ src/main.js
@@ -4,43 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { Amplify } from 'aws-amplify';
-import '@aws-amplify/ui-vue';
+import AmplifyVue from '@aws-amplify/ui-vue';
+import { config } from './services/amplify/config'
 
-
-const awsRegion = import.meta.env.VITE_AWS_REGION;
-const awsCognitoPool = import.meta.env.VITE_AWS_COGNITO_POOL
-const awsCognitoClient = import.meta.env.VITE_AWS_COGNITO_CLIENT
-const awsAppSyncHttp = import.meta.env.VITE_AWS_APPSYNC_ENDPOINT
-
-const envVars = [awsRegion, awsCognitoPool, awsCognitoClient, awsAppSyncHttp]
-
-envVars.forEach(v => {
-  if (!v) {
-    throw Error("Missing Env Variable");
-  }
-})
-
-// TODO: replace with .env
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      region: awsRegion,
-      userPoolId: awsCognitoPool,
-      userPoolWebClientId: awsCognitoClient,
-    },
-    mandatorySignIn: true
-  },
-  API: {
-    GraphQL: {
-      endpoint: awsAppSyncHttp,
-      region: awsRegion,
-      defaultAuthMode: 'userPool'
-    }
-  }
-})
+Amplify.configure(config)
 
 const app = createApp(App)
 
+app.use(AmplifyVue)
 app.use(router)
 
 app.mount('#app')
