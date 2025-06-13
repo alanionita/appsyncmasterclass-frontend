a30132570bc3d9f45e8ce3671d05c4d45cff356c
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-13 10:55:05 +0100

    FEAT (amplify-ui): handles correct sign in and create account flows
    Correctly renders ui when user is unauthenticated, authenticated, and configuring
    
    Correctly displays ui to sign in and create account
    
    Configures 'Create Account' form to use custom User Attributes for 'name', required by backend

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,83 @@
 <script setup>
+import { toRefs } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+const { route, user, authStatus, signOut, toSignUp } = toRefs(useAuthenticator());
+
+const formFields = {
+  signIn: {
+    username: {
+      placeholder: 'Enter your email',
+      label: 'Username (Email)'
+    },
+  },
+  signUp: {
+    username: {
+      type: 'username',
+      label: 'Username (Email) *',
+      placeholder: 'Please enter your email',
+      isRequired: true,
+      order: 1
+    },
+    name: {
+      type: 'name',
+      label: 'Name *',
+      placeholder: 'Please enter your name',
+      isRequired: true,
+      order: 1
+    },
+    password: {
+      type: 'password',
+      label: 'Password *',
+      placeholder: 'Please enter your password',
+      isRequired: true,
+    },
+    confirm_password: {
+      label: 'Confirm Password *',
+      isRequired: true,
+    },
+    phoneNo: {
+      type: 'phone_number',
+      label: 'Phone number *',
+      placeholder: 'Please enter your phone number +44...',
+      isRequired: true
+    }
+  }
+};
+
 </script>
 
 <template>
-  <header>
+  <template v-if="authStatus === 'unauthenticated'">
+    <authenticator :form-fields="formFields">
+      <template v-slot:sign-in-footer>
+        <div style="text-align: center">
+          <button @click="toSignUp" class="amplify-button amplify-field-group__control" data-fullwidth="false"
+            data-size="small" type="button" style="font-weight: normal">
+            Sign Up
+          </button>
+        </div>
+      </template>
+    </authenticator>
+  </template>
+  <template v-if="authStatus === 'configuring'">
+    <button @click="auth.signOut">Loading...</button>
+  </template>
+  <template v-if="authStatus === 'authenticated'">
+    <header>
       <nav class="nav">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-  </header>
+    </header>
 
-  <RouterView />
+    <h1>Hello {{ user.username }}!</h1>
+    <button @click="signOut">Sign out</button>
+
+    <RouterView />
+  </template>
 </template>
 
 <style scoped>
