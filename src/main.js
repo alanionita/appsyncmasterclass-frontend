4567c4090ba4ff4ef4adbba35d5987b39fbff6f9
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-13 13:37:34 +0100

    FEAT (main): adds logic to store Cognito token in session storage

--- src/main.js
+++ src/main.js
@@ -6,8 +6,12 @@ import router from './router'
 import { Amplify } from 'aws-amplify';
 import AmplifyVue from '@aws-amplify/ui-vue';
 import { config } from './services/amplify/config'
+import { sessionStorage } from 'aws-amplify/utils';
+import { cognitoUserPoolsTokenProvider } from 'aws-amplify/auth/cognito';
 
 Amplify.configure(config)
+
+cognitoUserPoolsTokenProvider.setKeyValueStorage(sessionStorage);
 
 const app = createApp(App)
 
