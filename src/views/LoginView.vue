26ae30ce416fa9922e31b0abadc636c4ba20de43
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-16 13:23:30 +0100

    FEAT (LoginView): adds authenticator UI

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,25 @@
-<template>
-    <div class="login">
-        <h1>This is an Login page</h1>
-    </div>
-</template>
+<script setup>
+import { toRefs } from 'vue';
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-vue";
+import { formFields } from '@/services/amplify/config';
+import "@aws-amplify/ui-vue/styles.css";
 
+const { toSignUp } = toRefs(useAuthenticator());
+
+</script>
+
+<template>
+    <authenticator :form-fields="formFields">
+        <template v-slot:sign-in-footer>
+            <div style="text-align: center">
+          <button @click="toSignUp" class="amplify-button amplify-field-group__control" data-fullwidth="false"
+            data-size="small" type="button" style="font-weight: normal">
+            Sign Up
+          </button>
+        </div>
+        </template>
+    </authenticator>
+</template>
 <style>
 @media (min-width: 1024px) {
     .login {
