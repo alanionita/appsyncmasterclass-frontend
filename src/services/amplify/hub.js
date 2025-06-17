673c85562d41200a9c77f2c78f38f6d0872ea3a5
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-17 16:27:49 +0100

    FEAT (services/amplify): adds hub file with definitions for listening and acting on auth events

---
+++ src/services/amplify/hub.js
@@ -0,0 +1,40 @@
+import { Hub } from 'aws-amplify/utils';
+import { getCurrentUser } from 'aws-amplify/auth';
+import router from '@/router';
+import { useAuthStore } from '@/stores/authentication';
+
+async function authListener({ payload }) {
+    const prefix = "Hub (auth): ";
+    const authStore = useAuthStore()
+    switch (payload.event) {
+        case 'signedIn':
+            console.info(prefix + 'user have been signedIn successfully.');
+            const user = await getCurrentUser();
+            authStore.login(user);
+            router.push('/home');
+            authStore.stopListener();
+            break;
+        case 'signedOut':
+            console.info(prefix + 'user have been signedOut successfully.');
+            break;
+        case 'tokenRefresh':
+            console.info(prefix + 'auth tokens have been refreshed.');
+            break;
+        case 'tokenRefresh_failure':
+            console.info(prefix + 'failure while refreshing auth tokens.');
+            break;
+        case 'signInWithRedirect':
+            console.info(prefix + 'signInWithRedirect API has successfully been resolved.');
+            break;
+        case 'signInWithRedirect_failure':
+            console.info(prefix + 'failure while trying to resolve signInWithRedirect API.');
+            break;
+        case 'customOAuthState':
+            logger.info('custom state returned from CognitoHosted UI');
+            break;
+    }
+}
+
+export function startAuthListener() {
+    return Hub.listen('auth', authListener)
+};
