b4b69bf84ed87353a75a14ee73fb6bf52db02117
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-17 16:29:51 +0100

    FEAT (LoginView): adds authStore, implements logic to start auth listener
    Minor tweaks to ui structure

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,24 +1,48 @@
 <script setup>
-import { toRefs } from 'vue';
+import { onMounted } from "vue";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-vue";
 import { formFields } from '@/services/amplify/config';
+import { useAuthStore } from '@/stores/authentication';
 import "@aws-amplify/ui-vue/styles.css";
 
-const { toSignUp } = toRefs(useAuthenticator());
+const authStore = useAuthStore();
+const { isPending, toSignUp, authStatus } = useAuthenticator();
+
+onMounted(() => {
+    if (!authStore.listener && !authStore.loggedIn) {
+        authStore.startListener();
+    }
+})
+
+async function handleSignOut() {
+    await authStore.logout();
+    authStore.startListener();
+}
 
 </script>
 
 <template>
-    <authenticator :form-fields="formFields">
-        <template v-slot:sign-in-footer>
+    <section>
+        <authenticator :form-fields="formFields">
+            <template v-if="authStatus === 'unauthenticated'" v-slot:sign-in-footer>
+                <div style="text-align: center">
+                    <button @click="toSignUp" class="amplify-button amplify-field-group__control" data-fullwidth="false"
+                        data-size="small" type="button" style="font-weight: normal">
+                        Sign Up
+                    </button>
+                </div>
+            </template>
+        </authenticator>
+        <template v-if="isPending">
             <div style="text-align: center">
-          <button @click="toSignUp" class="amplify-button amplify-field-group__control" data-fullwidth="false"
-            data-size="small" type="button" style="font-weight: normal">
-            Sign Up
-          </button>
-        </div>
+                <p>Loading...</p>
+            </div>
         </template>
-    </authenticator>
+        <template v-if="authStatus === 'authenticated'">
+            <h1>Hello {{ authStore.user?.username }}!</h1>
+            <button @click="handleSignOut">Sign out</button>
+        </template>
+    </section>
 </template>
 <style>
 @media (min-width: 1024px) {
