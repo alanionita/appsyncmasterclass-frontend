4385c883ed1602aac0543294aa88da6eb6c14a22
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-17 16:28:23 +0100

    FEAT (stores/auth): adds logic to start and stop Amplify Hub authListener

--- src/stores/authentication.js
+++ src/stores/authentication.js
@@ -1,29 +1,40 @@
 import { defineStore } from 'pinia';
-import AmplifyAuth from 'aws-amplify/auth';
+import * as AmplifyAuth from 'aws-amplify/auth';
 import router from '@/router';
+import { startAuthListener } from '@/services/amplify/hub';
 
 export const useAuthStore = defineStore('authentication', {
     state: () => ({
         loggedIn: false,
         user: undefined,
+        listener: null,
     }),
     actions: {
         login(user) {
-            this.user = user
+            this.loggedIn = true
+            if (user) {
+                this.user = user
+            }
         },
         async logout() {
             await AmplifyAuth.signOut({ global: true });
             this.loggedIn = false;
             this.user = undefined
             router.push('/')
+        },
+        startListener() {
+            this.listener = startAuthListener();
+        },
+        stopListener() {
+            if (this.listener && typeof this.listener === 'function') {
+                this.listener();
+                this.listener = null
+            }
         }
     },
     getters: {
-        loggedIn(state) {
-            return state.loggedIn
+        username({ user }) {
+            return user && user.username && user.username
         },
-        user(state) {
-            return state.user
-        }
     },
 });
