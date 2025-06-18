565fefe2315d27ac4e3712c66ce3905570b5cd22
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-18 08:23:48 +0100

    STYLE (App.vue): clear styles, clean up html

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,8 @@
 <script setup>
-import { toRefs } from 'vue';
-import { RouterLink, RouterView } from 'vue-router'
-import { Authenticator, useAuthenticator } from "@aws-amplify/ui-vue";
-import "@aws-amplify/ui-vue/styles.css";
-
-const { route, user, authStatus, signOut, toSignUp } = toRefs(useAuthenticator());
-
-const formFields = {
-  signIn: {
-    username: {
-      placeholder: 'Enter your email',
-      label: 'Username (Email)'
-    },
-  },
-  signUp: {
-    username: {
-      label: 'Username (Email) *',
-      placeholder: 'Please enter your email',
-      isRequired: true,
-      order: 1
-    },
-    name: {
-      label: 'Name *',
-      placeholder: 'Please enter your name',
-      isRequired: true,
-      order: 1
-    },
-    password: {
-      label: 'Password *',
-      placeholder: 'Please enter your password',
-      isRequired: true,
-    },
-    confirm_password: {
-      label: 'Confirm Password *',
-      isRequired: true,
-    },
-    phone_number: {
-      label: 'Phone number *',
-      placeholder: 'Please enter your phone number',
-      dialCodeList: ['+44'],
-      isRequired: true
-    }
-  }
-};
+import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <!-- <template v-if="authStatus === 'unauthenticated'">
-    <authenticator :form-fields="formFields">
-      <template v-slot:sign-in-footer>
-        <div style="text-align: center">
-          <button @click="toSignUp" class="amplify-button amplify-field-group__control" data-fullwidth="false"
-            data-size="small" type="button" style="font-weight: normal">
-            Sign Up
-          </button>
-        </div>
-      </template>
-    </authenticator>
-  </template>
-  <template v-if="authStatus === 'configuring'">
-    <button @click="auth.signOut">Loading...</button>
-  </template>
-  <template v-if="authStatus === 'authenticated'"> -->
-    <header>
-      <nav class="nav">
-        <RouterLink to="/">Root</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/home">Home</RouterLink>
-        <!-- <button @click="signOut">Sign out</button> -->
-      </nav>
-    </header>
-
-    <RouterView />
-  </template>
-<!-- </template>
-
-<style scoped>
-.nav {
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-}
-</style> -->
+  <RouterView />
+</template>
