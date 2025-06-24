d29e027217b4c1f25507dd184e43173de862a2b6
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-06-24 11:54:51 +0100

    FEAT (components): introduces 2 types of Input* components
    Improves reusability and composability, but creating InputText and InputPassword components
    
    Contain models an component specific features
    
    InputPassword contains show hide feature

---
+++ src/components/InputPassword.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { defineModel, defineProps, ref } from 'vue';
+
+const model = defineModel();
+const show = ref(false)
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+})
+
+function toggleShow() {
+  show.value = !show.value
+}
+
+</script>
+
+<template>
+  <label for="password" class="text-dark flex-2/3">{{ label }}</label>
+  <div class="flex justify-between items-center pr-4">
+    <input v-model="model" id="password" name="password" class="w-full bg-lightblue text-lg flex-1"
+      :type="`${show ? 'text' : 'password'}`">
+    <i :class="`${show ? 'fa-solid fa-eye-slash text-dark flex-0 text-2xl' : 'fa-solid fa-eye text-dark flex-0 text-2xl'}`"
+      @click="toggleShow"></i>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/InputText.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineModel, defineProps } from 'vue';
+
+const model = defineModel();
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  }
+})
+
+
+</script>
+
+<template>
+  <label for="name" class="text-dark">{{ label }}</label>
+  <input v-model="model" id="name" name="name" class="w-full bg-lightblue text-lg" type="text">
+</template>
+
+<style scoped></style>
