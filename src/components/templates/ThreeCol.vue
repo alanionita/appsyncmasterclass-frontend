a4ecbd5c190e60328a6b4d4c5e044983ce8cd4e8
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-07-30 14:48:53 +0100

    REFACTOR (HomeView): adds ThreeCol template view and implements it within HomeView
    Necessary because of upcoming creation of more pages that require the template

---
+++ src/components/templates/ThreeCol.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SideNav from '@/components/organisms/SideNav.vue'
+import DefaultRightBar from '@/components/organisms/DefaultRightBar.vue';
+
+</script>
+
+<template>
+  <main class="flex container h-screen w-screen m-auto gap-4 p-4">
+    <section class="flex-1/12 lg:flex-3/12 flex flex-col">
+      <SideNav />
+    </section>
+    <section class="flex-11/12 lg:flex-6/12 flex flex-col">
+      <slot name="middle"></slot>
+    </section>
+    <section class="hidden md:flex md:flex-col md:flex-3/12">
+      <DefaultRightBar />
+    </section>
+  </main>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup>
-import { store } from '@/store'
-import SideNav from '@/components/organisms/SideNav.vue'
-import DefaultRightBar from '@/components/organisms/DefaultRightBar.vue';
 import { useTwitterProfile } from '@/stores/twitterProfile';
 import { onMounted, ref } from 'vue';
 import { useAuthStore } from '@/stores/authentication';
 import { useTwitterTimeline } from '@/stores/twitterTimeline';
 import Timeline from '@/components/molecules/Timeline.vue';
+import ThreeColTemplate from '@/components/templates/ThreeCol.vue';
 
 const tweet = defineModel('tweet');
 
@@ -34,21 +32,9 @@ onMounted(() => loginUserIfAlreadyAuthenticated())
 </script>
 
 <template>
-  <main class="flex container h-screen w-screen m-auto gap-4 p-4">
-    <section class="flex-1/12 lg:flex-3/12 flex flex-col">
-      <SideNav></SideNav>
-    </section>
-    <section class="flex-11/12 lg:flex-6/12 flex flex-col">
-      <div v-if="store.pending">
-        <p>Loading data...</p>
-      </div>
-
-      <div v-else-if="store.error">
-        <h2>Error Loading Data</h2>
-        <p>{{ store.error }}</p>
-        <button @click="fetchData">Try Again</button>
-      </div>
-      <div class="flex h-full flex-col overflow-y-auto gap-4" v-else-if="profile">
+  <ThreeColTemplate>
+    <template #middle>
+      <div v-if="profile" class="flex h-full flex-col overflow-y-auto gap-4">
         <section class="border-b border-lighter flex items-center justify-between py-4">
           <h1 class="text-2xl font-semibold">Home</h1>
           <i class="far fa-star text-xl text-blue"></i>
@@ -69,19 +55,15 @@ onMounted(() => loginUserIfAlreadyAuthenticated())
                 <i class="text-lg text-blue far fa-chart-bar"></i>
                 <i class="text-lg text-blue far fa-smile"></i>
               </nav>
-              <button type="submit"
-                :disabled="!tweet"
+              <button type="submit" :disabled="!tweet"
                 class="sm:self-end h-12 px-4 text-white font-semibold bg-blue hover:bg-darkblue rounded-full disabled:cursor-not-allowed disabled:bg-lighter">Tweet</button>
             </div>
           </form>
         </section>
 
         <!-- timeline -->
-        <Timeline :tweets="timeline.tweets"/>
+        <Timeline :tweets="timeline.tweets" />
       </div>
-    </section>
-    <section class="hidden md:flex md:flex-col md:flex-3/12">
-      <DefaultRightBar />
-    </section>
-  </main>
+    </template>
+  </ThreeColTemplate>
 </template>
