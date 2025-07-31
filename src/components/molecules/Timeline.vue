b1ccfcf25d1682dd12db8ba3e3f8507f72bc4cb2
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-07-31 11:29:19 +0100

    FEAT (comp/Timeline): adds dynamic notice based on route

--- src/components/molecules/Timeline.vue
+++ src/components/molecules/Timeline.vue
@@ -2,22 +2,35 @@
 import TextButton from '@/components/atoms/TextButton.vue';
 import Tweet from '@/components/atoms/Tweet.vue';
 import Retweet from '@/components/atoms/Retweet.vue';
+import { useRoute } from 'vue-router';
 
 const { tweets } = defineProps(['tweets']);
+
+const route = useRoute();
+
+console.log({ route })
 
 </script>
 <template>
     <div>
-        <section v-if="tweets.length === 0" class="flex flex-col items-center justify-center w-full gap-4 px-4">
-            <p class="font-semibold text-lg">Welcome to Twitter!</p>
-            <p class="text-sm text-dark text-center">This is the best place to see what’s happening in your world. Find
-                some people and topics to follow now.</p>
-            <TextButton text="Let's go!" action="() => {}" />
+        <section v-if="tweets.length === 0">
+            <div v-if="route.name === 'home'" class="flex flex-col items-center justify-center w-full gap-4 mt-8 px-24 py-8">
+                <p class="font-semibold text-lg">Welcome to Twitter!</p>
+                <p class="text-sm text-dark text-center">This is the best place to see what’s happening in your world.
+                    Find
+                    some people and topics to follow now.</p>
+                <TextButton text="Let's go!" action="() => {}" />
+            </div>
+            <div class="flex flex-col items-center justify-center w-full gap-4 mt-8 px-4 py-8">
+                <p class="font-bold text-lg">You haven’t Tweeted yet</p>
+                <p class="text-sm text-dark">When you post a Tweet, it’ll show up here.</p>
+                <TextButton text="Tweet now" action="() => {}" />
+            </div>
         </section>
         <section v-if="tweets.length > 0" v-for="tweet in tweets" :key="tweet.id">
-            <Tweet v-if="!tweet.retweetOf" :tweet="tweet"/>
+            <Tweet v-if="!tweet.retweetOf" :tweet="tweet" />
 
-            <Retweet v-if="tweet.retweetOf" :tweet="tweet"/>
+            <Retweet v-if="tweet.retweetOf" :tweet="tweet" />
         </section>
     </div>
 </template>
