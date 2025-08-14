2a77a02e47f14318de8f8573cfbedb623044e904
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-08-14 16:09:46 +0100

    FEAT (stores): adds fetchSignedURL to twitterMyProfile store

--- src/stores/twitterMyProfile.js
+++ src/stores/twitterMyProfile.js
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia';
 import * as gql from '@/services/graphql/controllers'
 import { format } from 'date-fns';
+import { getUrl } from 'aws-amplify/storage';
+import { fetchAuthSession } from 'aws-amplify/auth';
 
 const defaultImgUrl = 'default_profile.png';
 const defaultCreatedAt = '1970-01-01';
@@ -39,16 +41,43 @@ export const useTwitterMyProfile = defineStore('twitterMyProfile', {
             }
         },
         async changeProfile(newProfile) {
-            if (!newProfile || !newProfile.name) throw Error("Missing required param")
+            try {
+                if (!newProfile || !newProfile.name) throw Error("Missing required param")
 
-            const profile = await gql.updateMyProfile(newProfile);
-            if (profile) {
-                const keys = Object.keys(profile);
-                for (let key of keys) {
-                    this[key] = profile[key]
+                const profile = await gql.updateMyProfile(newProfile);
+                if (profile) {
+                    const keys = Object.keys(profile);
+                    for (let key of keys) {
+                        this[key] = profile[key]
+                    }
                 }
+            } catch (err) {
+                console.error('Err [twitterMyProfile.changeProfile()', err.message)
+                throw err
             }
         },
+        async fetchSignedUrl(assetPath, stateKey = 'imgUrl') {
+            try {
+                const { userSub } = await fetchAuthSession();
+                if (!userSub) throw new Error('Not authenticated');
+
+                const validKeys = ['imgUrl'];
+
+                if (!validKeys.includes(stateKey)) throw new Error('Invalid state key');
+
+                const { url } = await getUrl({
+                    path: `${userSub}/${assetPath}`,
+                    options: {
+                        expiresIn: 300,
+                        accessLevel: 'private'
+                    }
+                });
+                this.imgUrl = url.toString();
+            } catch (err) {
+                console.error('Err [twitterMyProfile/fetchSignedUrl] ::', err.message)
+                console.info(JSON.stringify(err))
+            }
+        }
     },
     getters: {
         joinedDate: state => formatProfileCreatedAt(state),
