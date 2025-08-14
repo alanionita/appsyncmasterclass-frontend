c2da6274ddb2304147b602b44d9da2fb2877e60c
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-08-14 16:09:11 +0100

    FEAT (Amplify): implements s3 config

--- src/services/amplify/config.js
+++ src/services/amplify/config.js
@@ -1,9 +1,11 @@
 const awsRegion = import.meta.env.VITE_AWS_REGION;
 const awsCognitoPool = import.meta.env.VITE_AWS_COGNITO_POOL
 const awsCognitoClient = import.meta.env.VITE_AWS_COGNITO_CLIENT
+const awsCognitoIDP = import.meta.env.VITE_AWS_COGNITO_IDP
 const awsAppSyncEndpoint = import.meta.env.VITE_AWS_APPSYNC_ENDPOINT
+const awsS3Assets = import.meta.env.VITE_AWS_S3_ASSETS
 
-const envVars = [awsRegion, awsCognitoPool, awsCognitoClient, awsAppSyncEndpoint]
+const envVars = [awsRegion, awsCognitoPool, awsCognitoClient, awsAppSyncEndpoint, awsCognitoIDP, awsS3Assets]
 
 envVars.forEach(v => {
   if (!v) {
@@ -13,57 +15,66 @@ envVars.forEach(v => {
 
 
 export const config = {
-    Auth: {
-        Cognito: {
-            region: awsRegion,
-            userPoolId: awsCognitoPool,
-            userPoolClientId: awsCognitoClient,
-            authenticationFlowType: 'USER_SRP_AUTH', // Recommended flow
-        },
+  Auth: {
+    Cognito: {
+      region: awsRegion,
+      userPoolId: awsCognitoPool,
+      userPoolClientId: awsCognitoClient,
+      authenticationFlowType: 'USER_SRP_AUTH', // Recommended flow
+      identityPoolId: awsCognitoIDP
     },
-    API: {
-        GraphQL: {
-            endpoint: awsAppSyncEndpoint,
-            region: awsRegion,
-            defaultAuthMode: 'userPool'
-        }
+  },
+  API: {
+    GraphQL: {
+      endpoint: awsAppSyncEndpoint, 
+      region: awsRegion,
+      defaultAuthMode: 'userPool'
     }
+  },
+  Storage: {
+    S3: {
+      bucket: awsS3Assets, // Optional
+      region: awsRegion,
+      defaultAccessLevel: 'private',
+      cors: true // Ensures CORS headers are added
+    }
+  }
 }
 
 export const formFields = {
-    signIn: {
-      username: {
-        placeholder: 'Enter your email',
-        label: 'Username (Email)'
-      },
+  signIn: {
+    username: {
+      placeholder: 'Enter your email',
+      label: 'Username (Email)'
     },
-    signUp: {
-      username: {
-        label: 'Username (Email) *',
-        placeholder: 'Please enter your email',
-        isRequired: true,
-        order: 1
-      },
-      name: {
-        label: 'Name *',
-        placeholder: 'Please enter your name',
-        isRequired: true,
-        order: 1
-      },
-      password: {
-        label: 'Password *',
-        placeholder: 'Please enter your password',
-        isRequired: true,
-      },
-      confirm_password: {
-        label: 'Confirm Password *',
-        isRequired: true,
-      },
-      phone_number: {
-        label: 'Phone number *',
-        placeholder: 'Please enter your phone number',
-        dialCodeList: ['+44'],
-        isRequired: true
-      }
+  },
+  signUp: {
+    username: {
+      label: 'Username (Email) *',
+      placeholder: 'Please enter your email',
+      isRequired: true,
+      order: 1
+    },
+    name: {
+      label: 'Name *',
+      placeholder: 'Please enter your name',
+      isRequired: true,
+      order: 1
+    },
+    password: {
+      label: 'Password *',
+      placeholder: 'Please enter your password',
+      isRequired: true,
+    },
+    confirm_password: {
+      label: 'Confirm Password *',
+      isRequired: true,
+    },
+    phone_number: {
+      label: 'Phone number *',
+      placeholder: 'Please enter your phone number',
+      dialCodeList: ['+44'],
+      isRequired: true
     }
-  };
+  }
+};
