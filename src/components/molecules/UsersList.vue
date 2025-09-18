98a9f07ed63692b8f69c80b8c1412e10f6132d80
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-09-19 00:38:21 +0100

    REFACTOR (UserList): implements grid over flex containers
    Implements grid to consistently style the No results element, after using the grid within User component
    
    Consistency

--- src/components/molecules/UsersList.vue
+++ src/components/molecules/UsersList.vue
@@ -6,9 +6,18 @@ const { users } = defineProps(['users']);
 </script>
 
 <template>
-    <section class="py-4">
-        <span v-if="!users || users.length === 0" class="text-xl">No results</span>
-        <ul role="list" class="list-none" v-bind:key="user.id" v-for="user in users">
+    <section class="h-fit">
+        <div v-if="!users || users.length === 0" 
+            class="grid grid-cols-12 grid-rows-1 w-full p-4 gap-2 border-b border-lighter text-xl font-bold">
+            <section class="col-start-1 col-span-12 h-16 flex items-center">
+                <p class="align-middle">No results</p>
+            </section>
+        </div>
+        <ul v-if="users && users.length > 0" 
+            v-for="user in users"
+            v-bind:key="user.id" 
+            role="list" 
+            class="list-none">
             <User :user="user" />
         </ul>
     </section>
