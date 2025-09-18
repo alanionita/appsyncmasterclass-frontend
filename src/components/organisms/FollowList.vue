bd45ba219e8f0f9a02b8e5810249e7dce2d139cc
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-09-18 16:48:20 +0100

    FEAT (FollowList): adds combined component for handling following and followers
    Combines the two features into one dynamic component
    
    Use aria-selected and urls to choose between following and followers
    
    Url is used designate
    
    Adds validation on follower and followingCount(s)

---
+++ src/components/organisms/FollowList.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import * as Routes from '../../router/routeNames'
+import UsersList from '../molecules/UsersList.vue';
+
+const router = useRouter();
+const route = useRoute();
+const { profile } = defineProps(['profile'])
+const screenName = route.params.screenName
+const [followPath, ..._] =  route.path.split('/').reverse();
+
+const listCount = profile[`${followPath}Count`];
+const list = listCount && listCount > 0 && profile[`${followPath}`];
+const tabs = ref(['Following', 'Followers'])
+
+/** Navigates to the neighbouring list 
+ * @summary Navigates to corresponding list
+ * @param route - String of ['Profile', 'Following', 'Followers']
+ * */
+
+function goTo(route) {
+    router.push({
+        name: Routes[route],
+        params: {
+            screenName
+        }
+    })
+}
+
+</script>
+
+<template>
+
+    <div class="py-2 flex items-center">
+        <button @click="goTo('Profile')" class="rounded-full p-3 px-4 focus:outline-none hover:bg-lightblue">
+            <i class="fas fa-arrow-left text-blue"></i>
+        </button>
+        <div class="lg:block ml-4">
+            <h1 class="text-xl font-bold">{{ profile.name }}</h1>
+            <p class="text-left text-sm leading-tight text-dark">@{{ profile.screenName }}</p>
+        </div>
+    </div>
+
+    <ul class="flex flex-row justify-evenly mt-2 list-none" role="tablist">
+        <li v-for="(tab) in tabs" 
+            class="w-1/2 font-bold " 
+            role="presentation">
+            <button 
+                @click="goTo(tab)"
+                role="tab"
+                :aria-selected="route.name === tab && true"
+                class="w-full px-8 py-4 text-dark aria-selected:text-blue aria-selected:bg-lightblue font-bold border-b-2 hover:bg-lightblue">{{ tab }}</button>
+        </li>
+    </ul>
+
+    <UsersList :users="list" />
+</template>
