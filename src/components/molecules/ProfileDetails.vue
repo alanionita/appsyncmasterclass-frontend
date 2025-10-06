9b1259dc7923fb309b05eb52fc2dbf267e7942dc
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-10-06 22:38:23 +0100

    REFACTOR (ProfileDetails): adds conditional rendering for joinedDate, and implement <a> routes

--- src/components/molecules/ProfileDetails.vue
+++ src/components/molecules/ProfileDetails.vue
@@ -1,27 +1,6 @@
 <script setup>
 
-import { useRouter } from 'vue-router'
-import * as Routes from '../../router/routeNames';
-
-const router = useRouter();
 const { name, screenName, bio, location, website, joinedDate, followingCount, followersCount } = defineProps(['name', 'screen-name', 'bio', 'location', 'website', 'joinedDate', 'followingCount', 'followersCount']);
-
-function goToFollowing() {
-    router.push({
-        name: Routes.Following,
-        params: {
-            screenName
-        }
-    })
-}
-function goToFollowers() {
-    router.push({
-        name: Routes.Followers,
-        params: {
-            screenName
-        }
-    })
-}
 
 </script>
 
@@ -42,20 +21,20 @@ function goToFollowers() {
                 <a :href="`${website}`" target="_blank" class="text-dark">{{ website.replace('https://',
                     '').replace('http://', '') }}</a>
             </div>
-            <div class="flex flex-row gap-2">
+            <div v-if="joinedDate" class="flex flex-row gap-2">
                 <i class="far fa-calendar-alt text-dark align-text-bottom"></i>
                 <p class="text-dark">Joined {{ joinedDate }}</p>
             </div>
         </section>
         <section class="flex flex-row gap-8">
-            <button @click="goToFollowing()" class="flex flex-row hover:underline">
+            <a :href="`/${screenName}/following`" class="flex flex-row hover:underline">
                 <span class="font-bold">{{ followingCount }}</span>
                 <span class="text-dark whitespace-pre"> Following</span>
-            </button>
-            <button @click="goToFollowers()" class="flex flex-row hover:underline">
+            </a>
+            <a :href="`/${screenName}/followers`" class="flex flex-row hover:underline">
                 <span class="font-bold">{{ followersCount }}</span>
                 <span class="text-dark whitespace-pre"> Followers</span>
-            </button>
+            </a>
         </section>
     </section>
 </template>
