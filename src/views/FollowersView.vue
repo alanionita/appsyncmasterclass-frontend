105ce24a6d957ec08b7c3fe5f61b3a665ae8520d
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-11-03 10:46:15 +0000

    REFACTOR (views/Follow*): implements ui.ownProfile, removes verifyAuth

--- src/views/FollowersView.vue
+++ src/views/FollowersView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import FollowList from '@/components/organisms/FollowList.vue';
 import { onBeforeRouteUpdate, useRoute } from 'vue-router';
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 import { useAuthStore } from '@/stores/authentication';
 import { useTwitterMyProfile } from '@/stores/twitterMyProfile';
 import { useTwitterTheirProfile } from '@/stores/twitterTheirProfile';
@@ -10,32 +10,23 @@ import ThreeColTemplate from '@/components/templates/ThreeCol.vue';
 import { useUi } from '@/stores/ui';
 import { debounce } from '@/utils/timing';
 
-const path = ref(window.location.pathname)
 const route = useRoute()
 
 const myProfile = useTwitterMyProfile()
 const theirProfile = useTwitterTheirProfile()
-const timeline = useTwitterTimeline()
-const isMine = ref(false);
+// const timeline = useTwitterTimeline()
+const ui = useUi()
 const {loadingOn, loadingOff} = useUi()
-
-async function loginUserIfAlreadyAuthenticated() {
-    
-    const authStore = useAuthStore();
-    await authStore.verifyAuth(path);
-}
 
 async function updatePageData(screenName = null) {
     loadingOn()
-    if (!myProfile.isSelf(screenName)) {
-        isMine.value = false;
+    if (!ui.ownProfile) {
         await theirProfile.setProfile(screenName)
-        await timeline.getTweets(theirProfile.id)
+        // await timeline.getTweets(theirProfile.id)
         await theirProfile.getFollowers()
         await theirProfile.getFollowing()
     } else {
-        isMine.value = true;
-        await timeline.getMyTimeline()
+        // await timeline.getMyTimeline()
         await myProfile.getFollowers()
         await myProfile.getFollowing()
     }
@@ -43,7 +34,6 @@ async function updatePageData(screenName = null) {
 }
 
 onMounted(async () => {
-    await loginUserIfAlreadyAuthenticated()
     await updatePageData(route.params.screenName)
 })
 
@@ -55,10 +45,10 @@ onBeforeRouteUpdate(async (to, from) => {
 <template>
     <ThreeColTemplate>
         <template #middle>
-            <FollowList :profile="isMine ? myProfile : theirProfile"
-                :profile-name="isMine ? myProfile.name : theirProfile.name"
-                :profile-screen-name="isMine ? myProfile.screenName : theirProfile.screenName"
-                :list="isMine ? myProfile.followers : theirProfile.followers" />
+            <FollowList :profile="ui.ownProfile ? myProfile : theirProfile"
+                :profile-name="ui.ownProfile ? myProfile.name : theirProfile.name"
+                :profile-screen-name="ui.ownProfile ? myProfile.screenName : theirProfile.screenName"
+                :list="ui.ownProfile ? myProfile.followers : theirProfile.followers" />
         </template>
     </ThreeColTemplate>
 </template>
--- src/views/FollowingView.vue
+++ src/views/FollowingView.vue
@@ -2,10 +2,10 @@
 import FollowList from '@/components/organisms/FollowList.vue';
 import { onBeforeRouteUpdate, useRoute } from 'vue-router';
 import { onMounted, ref } from 'vue';
-import { useAuthStore } from '@/stores/authentication';
+// import { useAuthStore } from '@/stores/authentication';
 import { useTwitterMyProfile } from '@/stores/twitterMyProfile';
 import { useTwitterTheirProfile } from '@/stores/twitterTheirProfile';
-import { useTwitterTimeline } from '@/stores/twitterTimeline';
+// import { useTwitterTimeline } from '@/stores/twitterTimeline';
 import ThreeColTemplate from '@/components/templates/ThreeCol.vue';
 import { useUi } from '@/stores/ui';
 import { debounce } from '@/utils/timing';
@@ -15,27 +15,19 @@ const route = useRoute()
 
 const myProfile = useTwitterMyProfile()
 const theirProfile = useTwitterTheirProfile()
-const timeline = useTwitterTimeline()
+// const timeline = useTwitterTimeline()
+const ui = useUi();
 const { loadingOn, loadingOff } = useUi()
-const isMine = ref(false);
-
-async function loginUserIfAlreadyAuthenticated() {
-
-    const authStore = useAuthStore();
-    await authStore.verifyAuth(path);
-}
 
 async function updatePageData(screenName = null) {
     loadingOn()
-    if (!myProfile.isSelf(screenName)) {
-        isMine.value = false;
+    if (!ui.ownProfile) {
         await theirProfile.setProfile(screenName)
-        await timeline.getTweets(theirProfile.id)
+        // await timeline.getTweets(theirProfile.id)
         await theirProfile.getFollowers()
         await theirProfile.getFollowing()
     } else {
-        isMine.value = true;
-        await timeline.getMyTimeline()
+        // await timeline.getMyTimeline()
         await myProfile.getFollowers()
         await myProfile.getFollowing()
     }
@@ -43,7 +35,6 @@ async function updatePageData(screenName = null) {
 }
 
 onMounted(async () => {
-    await loginUserIfAlreadyAuthenticated()
     await updatePageData(route.params.screenName)
 })
 
@@ -55,11 +46,16 @@ onBeforeRouteUpdate(async (to, from) => {
 <template>
     <ThreeColTemplate>
         <template #middle>
-            <FollowList 
-                :profile="isMine ? myProfile : theirProfile" 
-                :profile-name="isMine ? myProfile.name : theirProfile.name"
-                :profile-screen-name="isMine ? myProfile.screenName : theirProfile.screenName"
-                :list="isMine ? myProfile.following : theirProfile.following"/>
+            <FollowList v-if="ui.ownProfile"
+                :profile="myProfile" 
+                :profile-name="myProfile.name"
+                :profile-screen-name="myProfile.screenName"
+                :list="myProfile.following"/>
+            <FollowList v-if="!ui.ownProfile"
+                :profile="theirProfile" 
+                :profile-name="theirProfile.name"
+                :profile-screen-name="theirProfile.screenName"
+                :list="theirProfile.following"/>
         </template>
     </ThreeColTemplate>
 </template>
