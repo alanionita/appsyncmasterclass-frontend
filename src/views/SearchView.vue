f553d80bf7615a9e32fe045112cf9eb308ee3774
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-11-03 17:22:07 +0000

    FEAT (SearchView): adds search mode toggle and behaviour
    Adds search modes ui elements
    
    Add behaviour where each mode change will trigger a new url and navigation

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -26,6 +26,22 @@ const handleSearch = debounce(() => {
     })
 }, SUBMIT_DELAY)
 
+const handleModeChange = debounce(() => {
+    router.push({
+        name: ROUTE_NAMES.Search,
+        query: {
+            q: query.value,
+            m: mode.value,
+            h: ulid()
+        }
+    })
+}, SUBMIT_DELAY)
+
+function changeMode(newMode) {
+    mode.value = newMode
+    handleModeChange()
+}
+
 onMounted(() => {
     if (route.query && route.query.q) {
         query.value = route.query.q
@@ -33,7 +49,6 @@ onMounted(() => {
     if (route.query && route.query.m) {
         mode.value = route.query.m
     }
-
 })
 
 </script>
@@ -49,15 +64,26 @@ onMounted(() => {
                     <section class="lg:block ml-4 w-full">
                         <i class="fas fa-search absolute mt-3 ml-5 text-m text-light"></i>
                         <label class="hidden" for="search-field">Search tweets</label>
-                        <input
-                            id="search-field"
+                        <input id="search-field"
                             class="pl-12 rounded-full w-full p-2 bg-lighter text-m focus:bg-white focus:outline-none focus:ring-2 focus:ring-blue"
-                            placeholder="Search Twitter" 
-                            type="search" 
-                            v-model="query"
+                            placeholder="Search Twitter" type="search" v-model="query"
                             v-on:keyup.enter="handleSearch()" />
                     </section>
                 </div>
+                <section class="grid grid-rows-1 grid-cols-5">
+                    <button
+                        class="col-span-1 row-span-1 text-dark font-bold border-b-2 md:px-8 md:py-4 hover:bg-lightblue">Top</button>
+                    <button @click="changeMode(SEARCH_MODES.latest)"
+                        class="col-span-1 row-span-1 text-dark font-bold border-b-2 md:px-5 md:py-4 hover:bg-lightblue"
+                        :class="`${mode == SEARCH_MODES.latest ? 'border-blue' : ''}`">{{ SEARCH_MODES.latest }}</button>
+                    <button @click="changeMode(SEARCH_MODES.people)"
+                        class="col-span-1 row-span-1 text-dark font-bold border-b-2 md:px-5 md:py-4 hover:bg-lightblue"
+                        :class="`${mode == SEARCH_MODES.people ? 'border-blue' : ''}`">{{ SEARCH_MODES.people }}</button>
+                    <button
+                        class="col-span-1 row-span-1 text-dark font-bold border-b-2 md:px-5 md:py-4 hover:bg-lightblue">Photos</button>
+                    <button
+                        class="col-span-1 row-span-1 text-dark font-bold border-b-2 md:px-5 md:py-4 hover:bg-lightblue">Videos</button>
+                </section>
                 <section>
                     <p>q: {{ $route.query.q }}</p>
                     <p>m: {{ $route.query.m }}</p>
