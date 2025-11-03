02ccf0d67db3cd2d0323b4690d37fb2fd3b7bc79
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-11-03 16:10:33 +0000

    FEAT (SearchBar): implements sidebar search
    Add SearchBar logic to type and navigate to /search
    
    Navigation adds query params: the query, mode (search mode), hash (ulid)
    
    State values implemented with refs and models

--- src/components/atoms/SearchBar.vue
+++ src/components/atoms/SearchBar.vue
@@ -1,6 +1,38 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { debounce } from '@/utils/timing';
+import * as Routes from '../../router/routeNames';
+import { ulid } from 'ulid'
+import { ref } from 'vue';
+
+const router = useRouter();
+const query = defineModel('');
+const searchModes = {
+    people: 'People',
+    latest: 'Latest'
+}
+const mode = ref(searchModes.latest)
+
+const SUBMIT_DELAY = 700
+
+const handleSearch = debounce(() => {
+    router.push({
+        name: Routes.Search,
+        query: {
+            q: query.value,
+            m: mode.value,
+            h: ulid()
+        }
+    })
+}, SUBMIT_DELAY)
+
+</script>
+
 <template>
     <section class="relative">
         <i class="fas fa-search absolute mt-3 ml-4 text-sm text-light"></i>
-        <input class="pl-10 rounded-full w-full p-2 bg-lighter text-sm" placeholder="Search Tweet" />
+        <label for="search-field"></label>
+        <input id="search-field" v-on:keyup="handleSearch" v-model="query"
+            class="pl-10 rounded-full w-full p-2 bg-lighter text-sm" placeholder="Search Tweet" />
     </section>
 </template>
