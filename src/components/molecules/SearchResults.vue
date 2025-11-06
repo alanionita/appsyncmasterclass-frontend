60ee693c5aebb5c877f29cbd81eb9b5b093c3769
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-11-06 17:19:48 +0000

    FIX (SearchResults): moves attributes off <template>

--- src/components/molecules/SearchResults.vue
+++ src/components/molecules/SearchResults.vue
@@ -1,21 +1,19 @@
 <script setup>
-
 import Tweet from '../atoms/Tweet.vue'
 import Retweet from '../atoms/Retweet.vue'
 import User from './User.vue'
-import { useSearch } from '@/stores/search';
-import { storeToRefs } from 'pinia';
+import { useSearch } from '@/stores/search'
+import { storeToRefs } from 'pinia'
 
 const storeSearch = useSearch()
 const { results } = storeToRefs(storeSearch)
-
 </script>
 <template>
-    <ul>
-        <template v-for="result in results" v-bind:key="result.id" role="list" class="list-none">
-            <User v-if="result.screenName" :user="result" />
-            <Retweet v-else-if="result.retweetOf" :tweet="result" />
-            <Tweet v-else :tweet="result" />
-        </template>
-    </ul>
+  <ul role="list" class="list-none">
+    <template v-for="result in results" v-bind:key="result.id">
+      <User v-if="result.screenName" :user="result" />
+      <Retweet v-else-if="result.retweetOf" :tweet="result" />
+      <Tweet v-else :tweet="result" />
+    </template>
+  </ul>
 </template>
