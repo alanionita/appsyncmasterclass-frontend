c56d9b5869b334c76e7ffdca117af524fb2702e6
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2025-11-20 20:52:34 +0000

    FEAT (NotificationList): adds logic to handle invalid image urls

--- src/components/molecules/NotificationsList.vue
+++ src/components/molecules/NotificationsList.vue
@@ -1,13 +1,29 @@
 <script setup>
 import NotificationsListEmpty from './NotificationsListEmpty.vue';
+import * as S3Urls from '@/services/s3/urls';
 
 const { notifications } = defineProps(["mode", "notifications"])
+
+async function handleImgUrlError({url, itemId}) {
+    try {
+        const errItemIdx = notifications.reduce((acc, item, index) => {
+            return item.id === itemId ? index : acc;
+        }, -1);
+        if (url && errItemIdx > 0) {
+            notifications[errItemIdx].profile.imgUrl = await S3Urls.refreshSignedUrl(url)
+        }
+    } catch (err) {
+        console.error('Err [NotificationList/handleImgUrlError] ::', err.message)
+        console.info(JSON.stringify(err))
+        return
+    }
+}
 
 </script>
 
 <template>
     <ul class="grid grid-cols-1 grid-rows-auto">
-        <NotificationsListEmpty v-if="!notifications || notifications.length === 0"/>
+        <NotificationsListEmpty v-if="!notifications || notifications.length === 0" />
 
         <li v-for="notification in notifications" :key="notification.id"
             class="grid grid-cols-1 grid-rows-2 md:p-4 gap-2 border-b border-dark hover:bg-lightest">
@@ -17,7 +33,10 @@ const { notifications } = defineProps(["mode", "notifications"])
                 <i v-if="notification.type == 'Retweeted'" class="fas fa-retweet text-green-500"></i>
 
                 <a :href="`/profile/${notification.profile.screenName}`">
-                    <img :src="`${notification.profile.imgUrl || 'default_profile.png'}`" class="size-8 rounded-full" />
+                    <img :src="`${notification.profile.imgUrl || 'default_profile.png'}`" @error="handleImgUrlError({
+                        itemId: notification.id,
+                        url: notification.profile.imgUrl
+                    })" class="size-8 rounded-full" />
                 </a>
             </section>
             <section class="flex items-center justify-between">
