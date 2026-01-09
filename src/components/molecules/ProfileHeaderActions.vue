97e1a1620545ff3c41b3e695aaa068e2aa60a5da
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2026-01-09 16:15:41 +0000

    REFACTOR (ProfileHeaderActions): replaces old structure with new <Image/>
    Removes prop requirements as a result
    
    Updates <Profile/> to no longer send those props

--- src/components/molecules/ProfileHeaderActions.vue
+++ src/components/molecules/ProfileHeaderActions.vue
@@ -1,31 +1,28 @@
 <script setup>
 import { useUi } from '@/stores/ui';
 import FollowButton from '../atoms/FollowButton.vue';
+import Image from '../atoms/Image.vue';
 
-const { imgUrl, toggleEditProfile, toggleSetupProfile, profile } = defineProps(['imgUrl', 'toggleSetupProfile', 'toggleEditProfile', 'profile'])
+const { toggleEditProfile, toggleSetupProfile, profile } = defineProps(['toggleSetupProfile', 'toggleEditProfile', 'profile'])
 
-const storeUi = useUi()
-
-async function handleImageError() {
-    if (profile) {
-        await profile.imgUrlSigned;
-    }
-}
+const storeUi = useUi();
 
 </script>
 
 <template>
     <section class="flex flex-row justify-between">
-        <img :src="imgUrl" @error="handleImageError" class="size-32 rounded-full border-white border-4"
-            style="margin-top:-80px" />
-
+        <!-- Required :key as a reactivity certainty hook -->
+        <Image 
+            :key="JSON.stringify(profile)"
+            :src="profile && profile.imgUrl" 
+            :classStr="`size-32 -mt-16 rounded-full border-white border-4`" />
         <!-- My Profile Actions -->
         <section v-if="storeUi.ownProfile" class="flex gap-4 py-4">
-            <button v-if="imgUrl === null || imgUrl === 'default_profile.png'" @click.prevent="toggleSetupProfile"
+            <button v-if="profile.imgUrl === null || profile.imgUrl === 'default_profile.png'" @click.prevent="toggleSetupProfile"
                 class="ml-auto text-blue font-bold px-4 py-2 rounded-full border border-blue hover:bg-lightblue">
                 Set up profile
             </button>
-            <button v-if="imgUrl !== null && imgUrl !== 'default_profile.png'" @click.prevent="toggleEditProfile"
+            <button v-if="profile.imgUrl !== null && profile.imgUrl !== 'default_profile.png'" @click.prevent="toggleEditProfile"
                 class="ml-auto text-blue font-bold px-4 py-2 rounded-full border border-blue hover:bg-lightblue">
                 Edit profile
             </button>
