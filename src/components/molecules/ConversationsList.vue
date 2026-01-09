d93c068ebf73ea87dbfac67d9fce7dc55ecf5396
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2026-01-09 00:15:54 +0000

    REFACTOR (ConversationsList): adds new active* state values from conversations store
    Refactors handleConversationClick to deal with new conversations (no messages)

--- src/components/molecules/ConversationsList.vue
+++ src/components/molecules/ConversationsList.vue
@@ -4,20 +4,27 @@ import { storeToRefs } from 'pinia';
 import Image from '../atoms/Image.vue';
 import LinkifyText from '../atoms/LinkifyText.vue';
 import { useConversations } from '@/stores/conversations';
+import { useUi } from '@/stores/ui';
 
 const storeConversations = useConversations();
-const { conversations, activeConversation } = storeToRefs(storeConversations)
+const storeUi = useUi();
+const { conversations, activeConversationId } = storeToRefs(storeConversations)
 const storeMessage = useMessages();
 
-async function handleConversationClick(id, otherUserId) {
-    storeConversations.setActive(id, otherUserId)
+async function handleConversationClick(_conversation) {
+    storeMessage.reset();
+    storeConversations.setActive(_conversation)
 
-    const selectedConv = storeConversations.find(id);
+    if (!_conversation.isNew) {
+        storeUi.toggleLoadingMessages();
+        const selectedConv = storeConversations.find(_conversation.id);
 
-    await storeMessage.list(selectedConv)
+        await storeMessage.list(selectedConv)
+        storeUi.toggleLoadingMessages();
+    }
 }
 
-function showNewBadge(conversation) { 
+function showNewBadge(conversation) {
     return conversation.hasNewMessages
 }
 
@@ -33,15 +40,14 @@ function showNewBadge(conversation) {
             Send a new message or wait until someone starts a conversation.
         </p>
     </section>
-    <ul v-else
-        role="list"
-        class="list-none h-screen w-full overflow-y-auto pb-80">
+    <ul v-else role="list" class="list-none h-full w-full overflow-y-auto">
         <li v-for="conversation in conversations" v-bind:key="`${conversation.id}-${conversation.lastModified}`"
             class="grid grid-cols-(--grid-cols-6-avatar) grid-rows-3 p-2 pl-4 border-b border-lighter hover:bg-lightest cursor-pointer data-active:bg-lightblue"
-            :data-active="conversation.id === activeConversation || undefined"
-            @click="handleConversationClick(conversation.id, conversation.otherUser.id)">
-            <a class="col-start-1 col-span-1 row-start-1 row-span-3 w-fit flex flex-col justify-center" :href="`#/${conversation.otherUser.screenName}`">
-                <Image :src="conversation.otherUser.imgUrl" :classStr="`h-12 w-12 rounded-full flex-none`"/>
+            :data-active="conversation.id === activeConversationId || undefined"
+            @click="handleConversationClick(conversation)">
+            <a class="col-start-1 col-span-1 row-start-1 row-span-3 w-fit flex flex-col justify-center"
+                :href="`/${conversation.otherUser.screenName}`">
+                <Image :src="conversation.otherUser.imgUrl" :classStr="`h-12 w-12 rounded-full flex-none`" />
             </a>
             <header class="col-start-2 col-span-4 row-start-1 row-span-2">
                 <p class="font-semibold">{{ conversation.otherUser.name }}</p>
@@ -51,15 +57,17 @@ function showNewBadge(conversation) {
 
             </header>
 
-            <LinkifyText :text="conversation.lastMessage" :classStr="`col-start-2 col-span-4 row-start-3 row-span-1 truncate`" />
+            <LinkifyText :text="conversation.lastMessage"
+                :classStr="`col-start-2 col-span-4 row-start-3 row-span-1 truncate`" />
             <aside class="col-start-6 col-span-1 row-span-3 grid grid-rows-subgrid justify-items-end">
                 <p class="row-start-1 row-span-1 text-sm text-dark">
                     {{ $filters.timeago(conversation.lastModified) }}
                 </p>
                 <figure v-if="showNewBadge(conversation)"
                     class="row-start-3 row-span-1 size-4 resize-both overflow-hidden self-end">
-                    <svg width="100%" height="100%" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">
-                        <circle cx="12" cy="12" r="12" fill="var(--color-blue)"/>
+                    <svg width="100%" height="100%" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="12" cy="12" r="12" fill="var(--color-blue)" />
                     </svg>
                 </figure>
             </aside>
