bd973b83f2bd0f1e3389c904d1e4477f8d7360d3
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2026-01-09 00:17:39 +0000

    FEAT (MessagesList): adds new UI element for showing an info message on new conversations
    Implements Linkify text in new element to link to profile, like Slack DMs
    
    Implements new state active* from store/conversations
    
    Adds new rendering rules and anchors for elements
    
    Centers the loader within the element, continues to show/hide too fast
    
    Used semantic grouping inside form

--- src/components/molecules/MessagesList.vue
+++ src/components/molecules/MessagesList.vue
@@ -7,12 +7,14 @@ import { useUi } from '@/stores/ui';
 import { onUpdated, ref } from 'vue';
 import { throwWithLabel } from '@/utils/error';
 import { useConversations } from '@/stores/conversations';
+import LinkifyText from '../atoms/LinkifyText.vue';
 
 const storeUi = useUi();
+const { loadingMessages } = storeToRefs(storeUi)
 const storeMessages = useMessages();
 const { messages, newMessage } = storeToRefs(storeMessages)
 const storeConversations = useConversations();
-const { activeOtherUserId } = storeToRefs(storeConversations)
+const { activeOtherUserId, activeConversationId, activeConversationIsNew, activeOtherUserScreenName } = storeToRefs(storeConversations)
 
 const disabled = ref(false);
 const mainFocus = ref(null);
@@ -37,8 +39,7 @@ onUpdated(() => {
 </script>
 
 <template>
-    <Loader v-if="storeUi.loadingMessages" />
-    <section v-else-if="storeMessages.size === 0" id="message-no-conversation-selecte-notice"
+    <section v-if="!activeConversationId" id="message-no-conversation-selecte-notice"
         class="flex flex-col px-8 py-12 border-b border-lighter text-xl font-bold">
         <p class="font-bold text-lg">You don't have a conversation selected</p>
         <p class="text-sm text-dark">This is where you’ll see messages from people you don’t follow. They
@@ -51,24 +52,34 @@ onUpdated(() => {
         </div>
 
     </section>
-    <section v-else class="h-screen flex flex-col">
-        <ul class="list-none w-full overflow-y-auto flex flex-col-reverse" role="list">
+    <section v-else class="h-full flex flex-col justify-between">
+        <div v-if="loadingMessages" class="flex flex-col h-full justify-center">
+            <Loader />
+        </div>
+        <ul v-else class="list-none w-full h-fit overflow-y-auto flex flex-col-reverse grow" role="list">
             <li v-for="message in messages" :key="message.messageId" :id="`message-${message.messageId}`"
                 class="grid grid-col-6 grid-rows-1 gap-y-4 p-4 hover:bg-lightest cursor-text">
                 <Message :message="message" />
             </li>
         </ul>
-        <form @submit.prevent="() => handleSendMessage(activeOtherUserId)"
-            class="bg-white p-4 border-t border-lighter flex w-full justify-center">
-            <label for="new-message" class="hidden">Enter new message</label>
-            <input ref="mainFocus" id="new-message" name="new-message"
-                class="pl-8 rounded-full w-full p-2 bg-lighter text-lg focus:bg-white focus:outline-none focus:ring-2 focus:ring-blue"
-                placeholder="Start a new message" type="search" v-model="newMessage" :disabled="disabled" />
-            <button class="rounded-full focus:outline-none right-4 size-16"
-                :class="`${newMessage.length == 0 ? 'opacity-50 cursor-default' : 'hover:bg-lightblue'}`"
-                :disabled="disabled" type="submit">
-                <i class="fas fa-arrow-right text-xl text-blue"></i>
-            </button>
+        <form @submit.prevent="() => handleSendMessage(activeOtherUserId)" class="flex flex-col">
+            <section v-if="activeConversationIsNew" class="py-8 px-4 text-l gap-2 flex flex-col bg-lightblue">
+                <LinkifyText :text="`This conversation is just between you and @${activeOtherUserScreenName}.`" />
+                <p>
+                    To learn more about them check out their profile.
+                </p>
+            </section>
+            <fieldset class="bg-white p-4 border-t h-[8vh] border-lighter flex w-full justify-center">
+                <label for="new-message" class="hidden">Enter new message</label>
+                <input ref="mainFocus" id="new-message" name="new-message"
+                    class="pl-8 rounded-full w-full p-2 bg-lighter text-lg focus:bg-white focus:outline-none focus:ring-2 focus:ring-blue"
+                    placeholder="Start a new message" type="search" v-model="newMessage" :disabled="disabled" />
+                <button class="rounded-full focus:outline-none right-4 size-16"
+                    :class="`${newMessage.length == 0 ? 'opacity-50 cursor-default' : 'hover:bg-lightblue'}`"
+                    :disabled="disabled" type="submit">
+                    <i class="fas fa-arrow-right text-xl text-blue"></i>
+                </button>
+            </fieldset>
         </form>
     </section>
 </template>
