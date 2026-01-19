81fac09d6cf9b3a376d1fdf3fd7ed22162c799eb
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2026-01-19 09:05:35 +0000

    FEAT (vite.config): adds minimal required PWA config
    Adds assets with <link>,<meta>,<title> to index.html
    
    Adds robots.txt
    
    vite.config - adds minimal recommended PWA setup, as documentated by the package

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +17,31 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    VitePWA({
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'appsyncmasterclass-frontend',
+        short_name: 'appsyncmasterclass-frontend',
+        description: 'Vue v3 + AWS AppSync app clone of former Twitter',
+        theme_color: '#4DBA87',
+        "start_url": ".",
+  "display": "standalone",
+  "background_color": "#000000",
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
